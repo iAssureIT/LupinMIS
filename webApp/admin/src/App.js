30ca34a47867b5ef3a889218c1c1e19a4c35d50f
priyankabhanvase2@gmail.com <MY_NAME@example.com>
2020-08-04 20:22:41 +0530

    Priyanka added Review changes in Reports

--- webApp/admin/src/App.js
+++ webApp/admin/src/App.js
@@ -8,11 +8,12 @@ import './coreAdmin/css/root.css'
 import LayoutSystemSecurity from './coreAdmin/LayoutSystemSecurity/LayoutSystemSecurity.js';
 import Layout from './coreAdmin/Layout/Layout.js';
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL; 
 // axios.defaults.baseURL  = "http://localhost:3054"
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-console.log("process.env.REACT_APP_BASE_URL = ", axios.defaultfaults.baseURL);
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+console.log("process.env.REACT_APP_BASE_URL = ", axios.defaults.baseURL);
+
 
 function App() {
   return (
