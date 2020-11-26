e36e2fdbeee2a3acad9b39d4d2a9d73e748c5d43
Prince <pkhandelwal7976>
2020-11-26 17:15:30 +0530

    Priyanka added changes.

--- webApp/centre/src/centres/plan/PlanDetails.js
+++ webApp/centre/src/centres/plan/PlanDetails.js
@@ -2,7 +2,7 @@ import React, { Component }   from 'react';
 import axios                  from 'axios';
 import swal                   from 'sweetalert';
 import _                      from 'underscore';
-import IAssureTable           from "../../coreAdmin/IAssureTable/IAssureTable.jsx";
+import IAssureTable           from "../../coreAdmin/IAssureTable/IAssureITTable.jsx";
 import Loader                 from "../../common/Loader.js";
 import BulkUpload             from "../bulkupload/BulkUpload.js";
 import $                      from 'jquery';
